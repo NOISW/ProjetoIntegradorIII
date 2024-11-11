ead6a74795037b912e4b4b68025c611919144091
NOISW <tecwcf@hotmail.com>
2024-11-11 09:53:45 -0300

    Correcao Links

--- api/excluir.php
+++ api/excluir.php
@@ -2,9 +2,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="./css/excluir.css">
+        <link rel="stylesheet" type="text/css" href="../css/excluir.css">
         <title>Escola Italiana Eugenio Montale</title>
-        <script src="js/funcoes.js"></script>
+        <script src="../js/funcoes.js"></script>
     </head>
     <body>
         <header>
