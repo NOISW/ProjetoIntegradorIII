2f00db6f68902c5c0f8c5b5499ab54b48bd9e954
NOISW <tecwcf@hotmail.com>
2024-11-11 10:10:09 -0300

    Conexao DB

--- api/conexao.php
+++ api/conexao.php
@@ -1,9 +1,9 @@
 <?php
 // Definir as credenciais de conexão com o banco de dados
-$servername = "localhost"; // Endereço do servidor MySQL (normalmente 'localhost')
-$username = "root"; // Nome de usuário do MySQL
-$password = "21212121"; // Senha do MySQL
-$dbname = "cadastro"; // Nome do banco de dados
+$servername = "pjt_integrador.mysql.dbaas.com.br"; // Endereço do servidor MySQL (normalmente 'localhost')
+$username = "pjt_integrador"; // Nome de usuário do MySQL
+$password = "Univesp@2022"; // Senha do MySQL
+$dbname = "pjt_integrador"; // Nome do banco de dados
 
 // Criar a conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
