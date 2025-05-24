d315d886b562e1b2811a735f23799380c2c924c4
NOISW <tecwcf@hotmail.com>
2025-05-24 15:55:55 -0300

    Update consulta.php

--- api/consulta.php
+++ api/consulta.php
@@ -15,6 +15,7 @@
             <a href="cadastrar.php">Cadastro</a>
             <a href="excluir.php">Retirada</a>
             <a href="reserva.php">Retirada</a>
+            <a href="local.php">Localização</a>
         </nav>        
         <section><br><br>
             <h2>Consulta de Produtos</h2><br>
